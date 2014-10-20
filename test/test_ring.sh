8160412f8c12cf371edbb4e495ce6b8dd2f3f841
mxg <mxg@sequencedsystems.com>
2014-10-20 20:28:06 +0800

    initial drop

---
+++ test/test_ring.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o nounset
+
+NUMREADER=50
+
+rm -f /dev/shm/ringtest
+(./rwr -f ringtest -n ${NUMREADER}; echo $$? > rv.w ) & touch rv.w
+for i in `seq ${NUMREADER}`; do
+    (./rrd -f ringtest ; echo $$? > rv.$$i ) & touch rv.$$i ;
+done
+
+for i in `seq ${NUMREADER}`; do
+    while [ ! -s rv.$$i ]; do sleep 1; done ;
+    if [ `cat rv.$$i` -ne 0 ]; then exit 1; fi ;
+done
+
+for i in `seq ${NUMREADER}`; do  rm rv.$$i ; done
+rm rv.w
+echo "*** ${NUMREADER} ring read/write test(s) passed ***"
+exit 0
---
+++ test/test_shmem_api.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o nounset
+
+NUMREADER=50
+
+rm -f /dev/shm/apitest
+(./apiwr -f apitest -n ${NUMREADER}; echo $$? > rv.w ) & touch rv.w
+for i in `seq ${NUMREADER}`; do
+    (./apird -f apitest ; echo $$? > rv.$$i ) & touch rv.$$i ;
+done
+
+for i in `seq ${NUMREADER}`; do
+    while [ ! -s rv.$$i ]; do sleep 1; done ;
+    if [ `cat rv.$$i` -ne 0 ]; then exit 1; fi ;
+done
+
+for i in `seq ${NUMREADER}`; do rm rv.$$i ; done
+rm rv.w
+echo "*** ${NUMREADER} api read/write test passed ***"
+exit 0

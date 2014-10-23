0a0c961a0dab8ffbeee75fadecc0cb2e9ea2223c
mxg <mxg@sequencedsystems.com>
2014-10-23 21:41:31 +0800

    removes erroneous $

--- test/test_shmem_api.sh
+++ test/test_shmem_api.sh
@@ -2,20 +2,20 @@
 
 set -o nounset
 
-NUMREADER=50
+NUMREADER=5
 
 rm -f /dev/shm/apitest
-(./apiwr -f apitest -n ${NUMREADER}; echo $$? > rv.w ) & touch rv.w
+(./apiwr -f apitest -n ${NUMREADER}; echo $? > rv.w ) & touch rv.w
 for i in `seq ${NUMREADER}`; do
-    (./apird -f apitest ; echo $$? > rv.$$i ) & touch rv.$$i ;
+    (./apird -f apitest ; echo $? > rv.$i ) & touch rv.$i ;
 done
 
 for i in `seq ${NUMREADER}`; do
-    while [ ! -s rv.$$i ]; do sleep 1; done ;
-    if [ `cat rv.$$i` -ne 0 ]; then exit 1; fi ;
+    while [ ! -s rv.$i ]; do sleep 1; done ;
+    if [ `cat rv.$i` -ne 0 ]; then exit 1; fi ;
 done
 
-for i in `seq ${NUMREADER}`; do rm rv.$$i ; done
+for i in `seq ${NUMREADER}`; do rm rv.$i ; done
 rm rv.w
 echo "*** ${NUMREADER} api read/write test passed ***"
 exit 0
